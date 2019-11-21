fae025cdbf838fe55c9dece634d8962628026b32
iqhater <iqhater@yandex.ru>
2019-11-21 21:25:01 +0300

    changed to default public path try

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/vue_router_vuex/" : "/",
+  // publicPath: process.env.NODE_ENV === "production" ? "/vue_router_vuex/" : "/",
   pluginOptions: {
     i18n: {
       locale: "en",
